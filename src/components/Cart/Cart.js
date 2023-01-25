2bd572b6257ae05d57b3aed21691c82b5fbec6de
Md. Habibullah <79191117+rakib333@users.noreply.github.com>
2023-01-26 00:16:52 +0600

    local storage commited

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,26 @@
 import React from "react";
 
 const Cart = ({ cart }) => {
+  let price = 0;
+  let shipping = 0;
+  let quantity = 0;
+  for (let product of cart) {
+    quantity = quantity + product.quantity;
+    price = price + product.price * product.quantity;
+    shipping = shipping + product.shipping;
+  }
+
+  // let tax = (price * .1).toFixed(2);
+  let tax = Math.round(price * 0.1);
   return (
     <div>
       <div className="order-details">
         <h2>Order Summery</h2>
         <div>
-          <h3>Added product:{cart.length} </h3>
-          <h3>Total price: </h3>
-          <h3>Total charge:</h3>
-          <h3>Tax</h3>
+          <h3>Added product:{quantity} </h3>
+          <h3>Total price:${price} </h3>
+          <h3>Total Shipping:${shipping}</h3>
+          <h3>Tax: ${tax}</h3>
           <h2>Grand Total: </h2>
         </div>
         <div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { addToDb, getStoredData } from "../../utilities/fakedb";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 
@@ -10,11 +11,25 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+    useEffect(() => {
+        const storedCart = getStoredData();
+        const savedCart = [];
+        for (const id in storedCart) {
+            const addedProduct = products.find(product => product.id === id);
+            if (addedProduct) {
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct)
+            }
+        }
+        setCart(savedCart)
+    },[products])
 
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
-  };
+    addToDb(product.id);
+    };
   return (
     <div className="shop-main">
       <div className="products-container">
