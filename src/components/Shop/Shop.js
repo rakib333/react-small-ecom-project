43d5cf14c1723ce94cc1b8ad1c51a7e386f1f1c7
Md. Habibullah <79191117+rakib333@users.noreply.github.com>
2023-01-28 02:38:32 +0600

    quantity problem fixed

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -25,10 +25,20 @@ const Shop = () => {
         setCart(savedCart)
     },[products])
 
-  const handleAddToCart = (product) => {
-    const newCart = [...cart, product];
+  const handleAddToCart = (selectedProduct) => {
+    let newCart = [];
+    const exists = cart.find(product => product.id === selectedProduct.id);
+    if (!exists) {
+      selectedProduct.quantity = 1;
+      newCart = [...cart, selectedProduct];
+    }
+    else {
+      const rest = cart.filter(product => product.id !== selectedProduct.id);
+      exists.quantity = exists.quantity + 1;
+      newCart = [...rest, exists];
+    }
     setCart(newCart);
-    addToDb(product.id);
+    addToDb(selectedProduct.id);
     };
   return (
     <div className="shop-main">
